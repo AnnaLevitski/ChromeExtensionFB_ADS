790bac5b59e8e31407078c90ac48807efe906bb3
AnnaLevitski <annalevitskiqa@gmail.com>
2023-03-04 19:29:02 +0200

    Started to do receive data

--- popup.js
+++ popup.js
@@ -1,1 +1,50 @@
-console.log("Here");
+let all = document.querySelector('body');
+all.style.font = 'caption';
+
+let btn = document.querySelector('.yellow_btn');
+btn.style.background = 'yellow';
+btn.style.marginTop = '40px';
+btn.style.borderRadius = '3px';
+
+let add = document.querySelector('.add');
+let addBtn = document.querySelector('.add_btn');
+let addBtnLast = add.lastElementChild;
+addBtn.style.marginBottom = '10px';
+
+function addClick(){
+    add.insertAdjacentHTML("beforeend", `<div> <input type="text" size="50">
+    <input type="submit" value="+" class="btn add_btn"> </div>`);
+    addBtnLast = add.lastElementChild;
+    addBtnLast.style.marginBottom = '10px';
+    addBtnLast.addEventListener( "click" , addClick);
+};
+addBtnLast.addEventListener( "click" , addClick);
+
+let text = document.querySelector('.text');
+let textPost;
+let photos = document.querySelector('.photo');
+let photoPost;
+
+
+
+function post (){
+    textPost = text.value;
+    console.log(textPost);
+    
+    for(const photo of photos.files){
+        console.log(photo);
+        console.log(photo.name);
+    };
+    
+}
+btn.addEventListener( "click" , post);
+
+function errorPrint(){
+    btn.insertAdjacentHTML("afterend", `<h2 class="red">Something went wrong</h2>`);
+    let errorPrintRed = document.querySelector('.red');
+    errorPrintRed.style.color = 'red';
+    
+    
+};
+
+errorPrint();
--- script.js
+++ script.js
@@ -1,4 +1,2 @@
 
 console.log("script in my extension");
-// let button = document.getElementById('btn');
-// button.style.background = 'red';
